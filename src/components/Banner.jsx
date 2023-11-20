2c88c7c54b6b19c6824e210d045ade5eeb431ca9
sapanabagde <sapana.bagde123@gmail.com>
2023-11-20 16:48:20 -0600

    Assignment #6 Orbit Report

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styling.css';
+
+function OrbitReport() {
+    return (
+        <div>
+            <h1>Orbit Report</h1>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    );
+}
+export default OrbitReport;
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,20 @@
+import React from 'react';
+import './styling.css';
+
+import satData from "./satData"
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div class='flex-container'>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>
+        All Orbits</button>
     </div>
   );
 };
